ea77c5f3459baf16696d3ec6086923eb3bf75c7a
arartemtem <hello@artemshestakov.ru>
2019-06-23 16:33:56 +0300

    ...

--- file_hosting.py
+++ file_hosting.py
@@ -4,3 +4,7 @@ from app.models import User, File, FilePath
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'File': File, 'FilePath': FilePath}
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8000', debug=True)
